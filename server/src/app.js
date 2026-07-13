79466ca886a7ee303008427864fd2c7b588c58e5
pr1368 <201082464+pr1368@users.noreply.github.com>
2026-07-13 11:48:04 +0330

    TWO

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,8 @@ const helmet = require("helmet");
 const compression = require("compression");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const routes = require("./routes");
+
 
 const app = express();
 
@@ -14,6 +16,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use("/api", routes);
 
 app.get("/", (req, res) => {
   res.json({
