66148a1228cf468965b922f6d0a5af5786da6159
pr1368 <201082464+pr1368@users.noreply.github.com>
2026-07-13 11:33:45 +0330

    FIRST

---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Rice Shop API is running 🚀",
+  });
+});
+
+module.exports = app;
